4e0a6ae34a628a742ff5fcb5a6937b5a828c2f7a
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-02-11 11:01:31 +0530

    a03s: switched to hs04

--- lineage_a03s.mk
+++ lineage_a03s.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/a03s/device.mk)
 
 # Inherit from hq-camera-samsung
-$(call inherit-product, vendor/samsung/hq-camera/hqcamera-samsung-hs03s.mk)
+$(call inherit-product, vendor/samsung/hq-camera/hqcamera-samsung-hs04.mk)
 
 TARGET_EXCLUDES_AUDIOFX := true
 
