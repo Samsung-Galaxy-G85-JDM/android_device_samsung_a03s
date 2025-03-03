7b20d739810576847a24cdf1cd591f83e539d1d7
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-03 07:56:58 +0530

    Update and rename orion_a03s.mk to superior_a03s.mk

--- superior_a03s.mk
+++ superior_a03s.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a03s/device.mk)
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/samsung/hq-camera/hqcamera-samsung-hs04.mk)
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_DEVICE := a03s
-PRODUCT_NAME := lineage_a03s
+PRODUCT_NAME := superior_a03s
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A03s
 PRODUCT_MANUFACTURER := samsung
