4a2033a1bfbe3034bbad02fab3c47bad0e0267bc
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-12 17:00:03 +0530

    Update and rename superior_a03s.mk to aosp_a03s.mk

--- aosp_a03s.mk
+++ aosp_a03s.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from a05m device
 $(call inherit-product, device/samsung/a03s/device.mk)
@@ -20,16 +20,13 @@ $(call inherit-product, vendor/samsung/hq-camera/hqcamera-samsung-hs04.mk)
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_DEVICE := a03s
-PRODUCT_NAME := superior_a03s
+PRODUCT_NAME := aosp_a03s
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A03s
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
-ORION_MAINTAINER := Smiley_9000
-ORION_BUILD_TYPE := Unofficial
-ORION_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 720
 WITH_GMS := true
 
