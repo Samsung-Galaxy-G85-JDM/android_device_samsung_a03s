94131ec40f0ea7abcf244789bfad78066e79148e
Samsung Galaxy JDM-MTK <sathnidu.2022597@ninaro.lk>
2025-03-12 17:01:29 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_a03s.mk
+    $(LOCAL_DIR)/aosp_a03s.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_a03s-user \
-    superior_a03s-userdebug \
-    superior_a03s-eng
+    aosp_a03s-user \
+    aosp_a03s-userdebug \
+    aosp_a03s-eng
